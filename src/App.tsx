a1b5732d8eca4fe0d66f510dc6268e14314e4bb8
MarcosPaul0 <marcosphip7@gmail.com>
2022-10-08 20:47:45 -0300

    feat: rotas do app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./themes/defaultTheme";
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyles />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
