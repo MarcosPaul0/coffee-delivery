69e9afa5d49d0cc992c92b64ef27e11a3e74c8dd
MarcosPaul0 <marcosphip7@gmail.com>
2023-02-07 19:52:00 -0300

    feat: atualização de contextos do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./themes/defaultTheme";
+import { CartContextProvider } from "./contexts/CartContext";
+import { ToastContainer } from "react-toastify";
 
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
-        <Router />
-        <GlobalStyles />
+        <CartContextProvider>
+          <Router />
+          <GlobalStyles />
+          <ToastContainer />
+        </CartContextProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
